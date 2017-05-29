814a0393587216f719cc07d445f0389596d59a2f
Software2 <software2@nospam.com>
2017-05-29 05:56:52 -0700

    Major code revision, added tons more functionality.
    *****This commit changes the public interface dramatically. It will break your existing scripts.*****

--- nanoleaf/Aurora.py
+++ nanoleaf/Aurora.py
@@ -1,110 +1,288 @@
 import requests
 import random
-import socket
-import select
-import time
 
 # Primary interface for an Aurora light
 # For instructions or bug reports, please visit
 # https://github.com/software-2/nanoleaf
 
+
 class Aurora(object):
-    def __init__(self, ipAddress, authToken):
-        self.baseUrl = "http://" + ipAddress + ":16021/api/v1/" + authToken + "/"
-        self.ipAddress = ipAddress
-        self.authToken = authToken
+    def __init__(self, ip_address: str, auth_token: str):
+        self.baseUrl = "http://" + ip_address + ":16021/api/v1/" + auth_token + "/"
+        self.ip_address = ip_address
+        self.auth_token = auth_token
 
     def __repr__(self):
-        return "<Aurora(" + self.ipAddress + ")>"
+        return "<Aurora(" + self.ip_address + ")>"
 
-    def __put(self, endpoint, data):
+    def __put(self, endpoint, data: dict) -> requests.request:
         url = self.baseUrl + endpoint
         try:
-            return requests.put(url, json = data)
+            r = requests.put(url, json=data)
         except requests.exceptions.RequestException as e:
             print(e)
             return
+        return self.__check_for_errors(r)
 
-    def __get(self, endpoint=None):
-        url = self.baseUrl
-        if (endpoint is not None):
-            url = self.baseUrl + endpoint
+    def __get(self, endpoint: str = "") -> requests.request:
+        url = self.baseUrl + endpoint
         try:
-            return requests.get(url)
+            r = requests.get(url)
         except requests.exceptions.RequestException as e:
             print(e)
             return
+        return self.__check_for_errors(r)
 
-    def getInfo(self):
+    def __delete(self, endpoint: str = "") -> requests.request:
+        url = self.baseUrl + endpoint
+        try:
+            r = requests.delete(url)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return
+        return self.__check_for_errors(r)
+
+    def __check_for_errors(self, r: requests.request) -> requests.request:
+        if r.status_code == 200:
+            if r.text == "": #BUG: Delete User returns 200, not 204 like it should, as of firmware 1.5.0
+                return None
+            return r.json()
+        elif r.status_code == 204:
+            return None
+        elif r.status_code == 403:
+            print("Error 400: Bad request! (" + self.ip_address + ")")
+        elif r.status_code == 401:
+            print("Error 401: Not authorized! This is an invalid token for this Aurora (" + self.ip_address + ")")
+        elif r.status_code == 404:
+            print("Error 404: Resource not found! (" + self.ip_address + ")")
+        elif r.status_code == 422:
+            print("Error 422: Unprocessible Entity (" + self.ip_address + ")")
+        elif r.status_code == 500:
+            print("Error 500: Internal Server Error (" + self.ip_address + ")")
+        else:
+            print("ERROR! UNKNOWN ERROR " + str(r.status_code)
+                  + ". Please post an issue on the GitHub page: https://github.com/software-2/nanoleaf/issues")
+        return None
+
+    ###########################################
+    # General functionality methods
+    ###########################################
+
+    @property
+    def info(self):
         """Returns the full Aurora Info request. 
         
         Useful for debugging since it's just a fat dump."""
-        r = self.__get()
-        print(r.text)
-        return r.json()
+        return self.__get()
 
+    @property
+    def color_mode(self):
+        """Returns the current color mode."""
+        return self.__get("state/colorMode")
 
+    def delete_user(self):
+        """CAUTION: Revokes your auth token from the device."""
+        self.__delete()
 
-    def getOn(self):
+    ###########################################
+    # On / Off methods
+    ###########################################
+
+    @property
+    def on(self):
         """Returns True if the device is on, False if it's off"""
         r = self.__get("state/on")
-        return r.json()["value"]
-    def on(self, value=True):
-        """Turns on the device. Optional param to turn off if you hate using the off function"""
+        if r is not None:
+            return r["value"]
+
+    @on.setter
+    def on(self, value: bool):
+        """Turns the device on/off. True = on, False = off"""
         data = {"on": value}
         self.__put("state", data)
+
+    @property
     def off(self):
-        """Turns off the device"""
-        data = {"on": False}
-        self.__put("state", data)
-    def toggleOn(self):
+        """Returns True if the device is off, False if it's on"""
+        return not self.on
+
+    @off.setter
+    def off(self, value: bool):
+        """Turns the device on/off. True = off, False = on"""
+        self.on = not value
+
+    def on_toggle(self):
         """Switches the on/off state of the device"""
-        self.on(not self.getOn())
+        self.on = not self.on
 
+    ###########################################
+    # Brightness methods
+    ###########################################
 
-
-    def getBrightness(self):
+    @property
+    def brightness(self):
         """Returns the brightness of the device (0-100)"""
         r = self.__get("state/brightness")
-        return r.json()["value"]
-    def getMaxBrightness(self):
-        """Returns the maximum brightness possible.
-        
-        No, I don't really know why..."""
-        r = self.__get("state/brightness")
-        return r.json()["max"]
-    def getMinBrightness(self):
-        """Returns the minimum brightness possible."""
-        r = self.__get("state/brightness")
-        return r.json()["min"]
+        return r["value"]
+
+    @brightness.setter
     def brightness(self, level):
         """Sets the brightness to the given level (0-100)"""
         data = {"brightness" : {"value": level}}
-        self.__put("state/brightness", data)
-    def brightnessRaise(self, level):
+        self.__put("state", data)
+
+    @property
+    def brightness_min(self):
+        """Returns the minimum brightness possible. (This always returns 0)"""
+        return self.__get("state/brightness/min")
+
+    @property
+    def brightness_max(self):
+        """Returns the maximum brightness possible. (This always returns 100)"""
+        return self.__get("state/brightness/max")
+
+    def brightness_raise(self, level):
         """Raise the brightness of the device by a relative amount (negative lowers brightness)"""
         data = {"brightness" : {"increment": level}}
-        self.__put("state/brightness", data)
-    def brightnessLower(self, level):
+        self.__put("state", data)
+
+    def brightness_lower(self, level):
         """Lower the brightness of the device by a relative amount (negative raises brightness)"""
-        self.brightnessRaise(-level)
+        self.brightness_raise(-level)
 
+    ###########################################
+    # Hue methods
+    ###########################################
 
+    @property
+    def hue(self):
+        """Returns the hue of the device (0-360)"""
+        r = self.__get("state/hue")
+        return r["value"]
 
-    def getEffect(self):
+    @hue.setter
+    def hue(self, level):
+        """Sets the hue to the given level (0-360)"""
+        data = {"hue" : {"value": level}}
+        self.__put("state", data)
+
+    @property
+    def hue_min(self):
+        """Returns the minimum hue possible. (This always returns 0)"""
+        return self.__get("state/hue/min")
+
+    @property
+    def hue_max(self):
+        """Returns the maximum hue possible. (This always returns 360)"""
+        return self.__get("state/hue/max")
+
+    def hue_raise(self, level):
+        """Raise the hue of the device by a relative amount (negative lowers hue)"""
+        data = {"hue" : {"increment": level}}
+        self.__put("state", data)
+
+    def hue_lower(self, level):
+        """Lower the hue of the device by a relative amount (negative raises hue)"""
+        self.hue_raise(-level)
+
+    ###########################################
+    # Saturation methods
+    ###########################################
+
+    @property
+    def saturation(self):
+        """Returns the saturation of the device (0-100)"""
+        r = self.__get("state/saturation")
+        return r["value"]
+
+    @saturation.setter
+    def saturation(self, level):
+        """Sets the saturation to the given level (0-100)"""
+        data = {"saturation" : {"value": level}}
+        self.__put("state", data)
+
+    @property
+    def saturation_min(self):
+        """Returns the minimum saturation possible. (This always returns 0)"""
+        return self.__get("state/saturation/min")
+
+    @property
+    def saturation_max(self):
+        """Returns the maximum saturation possible. (This always returns 100)"""
+        return self.__get("state/saturation/max")
+
+    def saturation_raise(self, level):
+        """Raise the saturation of the device by a relative amount (negative lowers saturation)"""
+        data = {"saturation" : {"increment": level}}
+        self.__put("state", data)
+
+    def saturation_lower(self, level):
+        """Lower the saturation of the device by a relative amount (negative raises saturation)"""
+        self.saturation_raise(-level)
+
+    ###########################################
+    # Color Temperature methods
+    ###########################################
+
+    @property
+    def color_temperature(self):
+        """Returns the color temperature of the device (0-100)"""
+        r = self.__get("state/ct")
+        return r["value"]
+
+    @color_temperature.setter
+    def color_temperature(self, level):
+        """Sets the color temperature to the given level (0-100)"""
+        data = {"ct" : {"value": level}}
+        self.__put("state", data)
+
+    @property
+    def color_temperature_min(self):
+        """Returns the minimum color temperature possible. (This always returns 1200)"""
+        #return self.__get("state/ct/min")
+        # BUG: Firmware 1.5.0 returns the wrong value.
+        return 1200
+
+    @property
+    def color_temperature_max(self):
+        """Returns the maximum color temperature possible. (This always returns 6500)"""
+        #return self.__get("state/ct/max")
+        #BUG: Firmware 1.5.0 returns the wrong value.
+        return 6500
+
+    def color_temperature_raise(self, level):
+        """Raise the color temperature of the device by a relative amount (negative lowers color temperature)"""
+        data = {"ct" : {"increment": level}}
+        self.__put("state", data)
+
+    def color_temperature_lower(self, level):
+        """Lower the color temperature of the device by a relative amount (negative raises color temperature)"""
+        self.color_temperature_raise(-level)
+
+    ###########################################
+    # Effect methods
+    ###########################################
+
+    @property
+    def effect(self):
         """Returns the active effect"""
-        r = self.__get("effects/select")
-        return r.json()
-    def getEffects(self):
-        """Returns a list of all effects stored on the device"""
-        r = self.__get("effects/effectsList")
-        return r.json()
+        return self.__get("effects/select")
+
+    @effect.setter
     def effect(self, effectName):
-        """Sets the active effect to the name specifiec"""
+        """Sets the active effect to the name specified"""
         data = {"select" : effectName}
         self.__put("effects", data)
-    def effectRandom(self):
+
+    @property
+    def effects_list(self):
+        """Returns a list of all effects stored on the device"""
+        return self.__get("effects/effectsList")
+
+    def effect_random(self):
         """Sets the active effect to a new random effect stored on the device"""
-        effectList = self.getEffects()
-        effectList.remove(self.getEffect())
-        self.effect(random.choice(effectList))
+        effect_list = self.effects_list
+        active_effect = self.effect
+        if active_effect != "*Solid*":
+            effect_list.remove(self.effect)
+        self.effect = random.choice(effect_list)
--- nanoleaf/Setup.py
+++ nanoleaf/Setup.py
@@ -7,6 +7,7 @@ import time
 # For instructions or bug reports, please visit
 # https://github.com/software-2/nanoleaf
 
+
 def find_auroras(seek_time: float = 30):
     """
     Returns a list of the IP addresses of all Auroras found on the network
@@ -26,18 +27,19 @@ def find_auroras(seek_time: float = 30):
            'MX: ' + str(SSDP_MX)]
     req = '\r\n'.join(req).encode('utf-8')
 
-    auroraLocations = []
-    def checkIfNewAurora(response):
-        if not SSDP_ST in response:
+    aurora_locations = []
+    
+    def check_if_new_aurora(r):
+        if SSDP_ST not in r:
             return
-        for line in response.split("\n"):
+        for line in r.split("\n"):
             if "Location:" in line:
-                newLocation = line.replace("Location:", "").strip() \
+                new_location = line.replace("Location:", "").strip() \
                                   .replace("http://", "") \
                                   .replace(":16021", "")
-                if not newLocation in auroraLocations:
-                    auroraLocations.append(newLocation)
-                    print("New Aurora found at " + newLocation)
+                if new_location not in aurora_locations:
+                    aurora_locations.append(new_location)
+                    print("New Aurora found at " + new_location)
                 return
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -53,34 +55,39 @@ def find_auroras(seek_time: float = 30):
             ready = select.select([sock], [], [], 5)
             if ready[0]:
                 response = sock.recv(1024).decode("utf-8")
-                checkIfNewAurora(response)
+                check_if_new_aurora(response)
         except socket.error as err:
             print("Socket error while discovering SSDP devices!")
             print(err)
-            print("If you are sure your network connection is working, please post an issue on the GitHub page: https://github.com/software-2/nanoleaf/issues")
-            print("Please include as much information as possible, including your OS, how your computer is connected to your network, etc.")
+            print("If you are sure your network connection is working, "
+                  "please post an issue on the GitHub page: https://github.com/software-2/nanoleaf/issues")
+            print("Please include as much information as possible, including your OS, "
+                  "how your computer is connected to your network, etc.")
             sock.close()
             break
 
-    if len(auroraLocations) == 0:
+    if len(aurora_locations) == 0:
         print("Discovery complete, but no Auroras found!")
-        return auroraLocations
-    print("Discovery complete! Found " + str(len(auroraLocations)) + " Auroras.")
-    return auroraLocations
+        return aurora_locations
+    print("Discovery complete! Found " + str(len(aurora_locations)) + " Auroras.")
+    return aurora_locations
 
-def generate_auth_token(self, ipAddress: str):
-    '''
+
+def generate_auth_token(ip_address: str):
+    """
     Generates an auth token for the Aurora at the given IP address. 
     
-    You must first press and hold the power button on the Aurora for about 5-7 seconds, until the white LED flashes briefly.
-    '''
-    url = "http://" + ipAddress + ":16021/api/v1/new"
+    You must first press and hold the power button on the Aurora for about 5-7 seconds, 
+    until the white LED flashes briefly.
+    """
+    url = "http://" + ip_address + ":16021/api/v1/new"
     r = requests.post(url)
     if r.status_code == 200:
-        print("Auth token for "  + ipAddress + " successfully generated!   " + str(r.json()))
+        print("Auth token for " + ip_address + " successfully generated!  " + str(r.json()))
         return r.json()['auth_token']
     if r.status_code == 401:
-        print("Not Authorized! I don't even know how this happens. Please post an issue on the GitHub page: https://github.com/software-2/nanoleaf/issues")
+        print("Not Authorized! I don't even know how this happens. "
+              "Please post an issue on the GitHub page: https://github.com/software-2/nanoleaf/issues")
     if r.status_code == 403:
         print("Forbidden! Press and hold the power button for 5-7 seconds first! (Light will begin flashing)")
     if r.status_code == 422:
